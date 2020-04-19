57789b5fbc778165d3655a102748b39481710f8c
mariano_venesio <mvenesio@unitedvirtualities.com>
2020-04-19 16:36:46 -0300

    create footernavbar

---
+++ Components/FooterNavbar/FooterNavbar.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+import PlusSquare from '../Icons/PlusSquare';
+import ChartBar from '../Icons/ChartBar';
+import FileRegular from '../Icons/FileRegular';
+
+import Link from 'next/link';
+
+const FooterNavbarContainer = styled.div`
+    -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+    cursor:pointer;
+    height:10%;
+    width:100%;
+    background-color:${props => props.theme.color.primaryDarkColor};
+    grid-template-columns: 1fr 1fr 2fr 1fr 1fr;
+    font-family: ${props => props.theme.font.family};
+    font-size: ${props => props.theme.font.size.subTitle};
+    color:${props => props.theme.color.white};
+    display:flex;
+    flex-direction:row;
+    justify-content:center;
+`;
+const PlusButton = styled(PlusSquare)`
+    align-self:center;
+    font-size: 40px;
+    color: black;
+    margin: 0rem 1rem 0rem 1rem;
+    &:hover{
+        color: ${props => props.theme.color.primaryLightColor};
+    }
+`;
+const HistoryButton = styled(FileRegular)`
+    align-self:center;
+    font-size: 40px;
+    color: black;
+    margin: 0rem 1rem 0rem 1rem;
+    &:hover{
+        color: ${props => props.theme.color.primaryLightColor};
+    }
+`;
+const ChartButton = styled(ChartBar)`
+    align-self:center;
+    font-size: 40px;
+    color: black;
+    margin: 0rem 1rem 0rem 1rem;
+    &:hover{
+        color: ${props => props.theme.color.primaryLightColor};
+    }
+`;
+
+const FooterNavbar = () => {
+    return (
+        <FooterNavbarContainer>
+            <PlusButton onClick={() => console.log('111111')}/>
+            <HistoryButton onClick={() => console.log('2222')}/>
+            <ChartButton onClick={() => console.log('3333')}/>
+        </FooterNavbarContainer>
+    )
+}
+
+export default FooterNavbar
+
+
