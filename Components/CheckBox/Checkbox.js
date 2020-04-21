fc41847c9a5ad8db57c668ce627dd55aaebb6605
mariano_venesio <mvenesio@unitedvirtualities.com>
2020-04-21 14:12:10 -0300

    refactor checkbox

--- Components/CheckBox/Checkbox.js
+++ Components/CheckBox/Checkbox.js
@@ -1,10 +1,56 @@
+// @ts-nocheck
+import React from 'react';
 import styled from 'styled-components';
+import Circle from '../Icons/CircleSolid';
+import Check from '../Icons/CheckCircle';
 
-const Checkbox = styled.input.attrs({type: 'checkbox'})`
-    width:10%;
-    box-sizing: border-box;
-    line-height: 1.2em;
-    height:25px;
-    outline: unset;
+const CheckBoxContainer = styled.label `
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  font-family: ${props => props.theme.font.family};
+  position:relative;
+  margin:0rem 1rem 0rem 1rem;
 `;
-export default Checkbox;
+const CircleIcon = styled(Circle)`
+    color: ${props =>  props.theme.color.primaryLightColor};
+    position:absolute;
+    top:2.5px;
+    left:2.5px;
+`;
+const CircleCheck = styled(Check)`
+    color: ${props => props.checked ? props.theme.color.primaryDarkColor :  props.theme.color.primaryLightColor};
+    position:absolute;
+    top:2.5px;
+    left:2.5px;
+`;
+
+const CheckBoxInput = styled.input.attrs({type: 'checkbox'})`
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+`;
+
+const CheckBox = (props) => {
+    const { title, onCheck, checked } = props;
+
+    return (
+      <CheckBoxContainer >
+        {title}
+        <CheckBoxInput onChange={onCheck} checked={checked} />
+        <CircleIcon/>
+        <CircleCheck checked={checked}/>
+      </CheckBoxContainer>
+    );
+}
+
+export default CheckBox;
