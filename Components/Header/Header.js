5671f1d44303d981184126528c3cc88eed2609f1
mariano_venesio <mvenesio@unitedvirtualities.com>
2020-04-23 18:27:51 -0300

    add log out button

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
+import {useRouter} from 'next/router';
+import PowerOff from '../Icons/PowerOff';
 
 const HeaderContainer = styled.div`
     -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
     cursor:pointer;
     display:flex;
+    flex-direction:row;
+    justify-content:space-between;
     width:100%;
     height:10%;
     background-color:${props => props.theme.color.primaryDarkColor};
@@ -18,16 +22,30 @@ const HeaderText = styled.div`
     align-self:center;
     padding-left: 1rem;
 `;
+const LogOutButton = styled(PowerOff)`
+    align-self:center;
+    font-size: 40px;
+    color: white;
+    margin: 0rem 1rem 0rem 1rem;
+    &:hover{
+        color: ${props => props.theme.color.primaryLightColor};
+    }
+`;
 
 const Header = (props) => {
     const {title} = props;
+    const router = useRouter();
     return (
         <HeaderContainer>
-            <Link href={'./index'}>
+            <Link href={'./Homepage'}>
                 <HeaderText>
                     {title}
                 </HeaderText>
             </Link>
+            {
+                (router.pathname !== './' && router.pathname !== './index') &&
+                <LogOutButton onClick={() => {localStorage.removeItem('token'); router.push('/')}} />
+            }
         </HeaderContainer>
     )
 }
