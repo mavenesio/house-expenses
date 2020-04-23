e3edc12493ae59ae46555d700ce14b3e0faf5bd6
mariano_venesio <mvenesio@unitedvirtualities.com>
2020-04-23 18:26:58 -0300

    add apollo configuration

--- config/apollo.js
+++ config/apollo.js
@@ -1,14 +1,29 @@
-import {ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import {ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import fetch from 'node-fetch';
+import {setContext} from 'apollo-link-context';
 
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000/',
+    // @ts-ignore
+    fetch
+});
+
+const authLink = setContext((_, {headers}) => {
+    
+    const token = localStorage.getItem('token');
+    
+    return {
+        headers:{
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        }
+    }
+});
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
-        uri: 'http://localhost:4000/',
-        // @ts-ignore
-        fetch
-    })
+    // @ts-ignore
+    link: authLink.concat(httpLink),
 });
 
 
