d211f56eb4d8814691ebad51f0539a67a896e925
mariano_venesio <mvenesio@unitedvirtualities.com>
2020-04-24 19:53:22 -0300

    change blur background color

--- Components/Modal/Modal.js
+++ Components/Modal/Modal.js
@@ -10,7 +10,7 @@ const ModalContainer = styled.div`
     top:0;
     left:0;
     right:0;
-    background-color:rgba(222, 222, 222,0.5);
+    background-color:rgba(0, 0, 0,.5);
     width:100%;
     height:100%;
 `;
