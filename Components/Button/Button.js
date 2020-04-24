4250c3c19450992ba8f54c3eb892b20d4700d5b1
mariano_venesio <mvenesio@unitedvirtualities.com>
2020-04-24 15:27:11 -0300

    add secondary button

--- Components/Button/Button.js
+++ Components/Button/Button.js
@@ -1,15 +1,37 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
+    cursor: pointer;
     padding: 1rem;
     margin-top: 1rem;    
     margin-bottom: 1rem;
     border-radius: 8px;
-    min-width:200px;
-    width:10vw;
-    color: ${props => props.theme.color.primaryDarkColor};
     border: 2px solid ${props => props.theme.color.primaryDarkColor};
     background-color: ${props => props.theme.color.primaryColor};
+    font-size: ${props => props.theme.font.size.text};
+    font-weight:${props => props.theme.font.weight.bold};
+    color:white;
+    &:hover{
+        background-color: ${props => props.theme.color.primaryDarkColor};
+        color: ${props => props.theme.color.primaryLightColor};
+    }
+    &:focus {
+        outline: unset;
+    }
+    &:disabled {
+        background-color: ${props => props.theme.color.primaryDarkColor};
+        color: ${props => props.theme.color.primaryLightColor};
+    }
+`;
+export const SecondaryButton = styled.button`
+    cursor: pointer;
+    padding: 1rem;
+    margin-top: 1rem;    
+    margin-bottom: 1rem;
+    border-radius: 8px;
+    color: ${props => props.theme.color.primaryDarkColor};
+    border: 2px solid ${props => props.theme.color.primaryDarkColor};
+    background-color: white;
     font-size: ${props => props.theme.font.size.text};
     font-weight:${props => props.theme.font.weight.bold};
     &:hover{
