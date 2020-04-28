358043e0251247710468954b2c338f484a395fa5
mariano_venesio <mvenesio@unitedvirtualities.com>
2020-04-27 22:24:30 -0300

    delete comment

--- config/apollo.js
+++ config/apollo.js
@@ -4,7 +4,6 @@ import {setContext} from 'apollo-link-context';
 
 const httpLink = createHttpLink({
     uri: process.env.URI,
-    //uri: 'http://localhost:4000/',
     // @ts-ignore
     fetch
 });
