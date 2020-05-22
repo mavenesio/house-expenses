e97b7bea8247d704fe85e284c439d1faf37265c5
mariano_venesio <mvenesio@unitedvirtualities.com>
2020-05-22 17:45:05 -0300

    Change spinner

--- Components/Spinner/Spinner.js
+++ Components/Spinner/Spinner.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import {PacmanLoader} from "react-spinners";
+import {SyncLoader} from "react-spinners";
 
 const SpinnerContainer = styled.div`
     position: absolute;
@@ -10,13 +10,14 @@ const SpinnerContainer = styled.div`
     width:100%;
     height:calc(100%);
     top:0px;
-    right:0px;
+    left:0px;
     background-color:rgba(245, 245, 245,0.3);
 `;
 const SpinnerWrapper = styled.div`
     position:relative;    
-    top: 100px;
-    left: calc(30%);
+    top: 150px;
+    width: 200px;
+    left: calc(50% - 100px);
     @media (max-width: 768px) {
         left: 0px;
     }
@@ -27,7 +28,7 @@ const Spinner = ({loading}) => (
                 loading &&
                 <SpinnerContainer>
                     <SpinnerWrapper>
-                        <PacmanLoader size={90} color='#006064' loading={loading}/>
+                        <SyncLoader size={50} color='#42404d' loading={loading}/>
                     </SpinnerWrapper>
                 </SpinnerContainer>
             }
